8c05d312b8da5eeaab567434c5b73e59097e533e
wangyue-gagua <2503308794@qq.com>
2021-09-12 16:19:04 +0800

    example: animation fade-in

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math';
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: RandomWords(),
+      home: FadeInDemo(),
     );
   }
 }
@@ -114,5 +113,45 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 }
+
+// Fade-in
+
+const owl_url = 'https://raw.githubusercontent.com/flutter/website/master/src/images/owl.jpg';
+
+class FadeInDemo extends StatefulWidget {
+  _FadeInDemoState createState() => _FadeInDemoState();
+}
+
+class _FadeInDemoState extends State<FadeInDemo> {
+  double opacity = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Image.network(owl_url),
+      TextButton(
+        child: Text(
+          'Show details',
+          style: TextStyle(color: Colors.blueAccent),
+        ),
+        onPressed: () => setState(() {
+          opacity = 1;
+        }),
+      ),
+      AnimatedOpacity(
+        duration: Duration(seconds: 2),
+        opacity: opacity,
+        child: Column(
+          children: <Widget>[
+            Text('Type: Owl'),
+            Text('Age: 39'),
+            Text('Employment: None'),
+          ],
+        ),
+      )
+    ]);
+  }
+}
+
 
 

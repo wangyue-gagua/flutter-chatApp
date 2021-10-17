74e82ab47c2d82ba1b20a0e317685e15b6f9c1bb
wangyue-gagua <2503308794@qq.com>
2021-10-18 00:14:47 +0800

    add progress bar animation

--- lib/webApp.dart
+++ lib/webApp.dart
@@ -86,9 +86,7 @@ class _SignUpFormState extends State<SignUpForm> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            LinearProgressIndicator(
-              value: _formProgress,
-            ),
+            AnimatedProgressIndicator(value: _formProgress),
             Text(
               'Sign up',
               style: Theme.of(context).textTheme.headline4,
@@ -129,5 +127,58 @@ class _SignUpFormState extends State<SignUpForm> {
                 ))
           ],
         ));
+  }
+}
+
+class AnimatedProgressIndicator extends StatefulWidget {
+  final double value;
+
+  AnimatedProgressIndicator({
+    required this.value,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnimatedProgressIndicatorState();
+  }
+}
+
+class _AnimatedProgressIndicatorState
+    extends State<AnimatedProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double> _curveAnimation;
+
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1200));
+
+    final colorTween = TweenSequence([
+      TweenSequenceItem(tween: ColorTween(begin: Colors.red ,end: Colors.orange), weight: 1),
+      TweenSequenceItem(tween: ColorTween(begin: Colors.orange, end: Colors.yellow), weight: 1),
+      TweenSequenceItem(tween: ColorTween(begin: Colors.yellow, end: Colors.green), weight: 1),
+      
+    ]);
+    
+    _colorAnimation = _controller.drive(colorTween);
+    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
+  }
+
+  @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.animateTo(widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return AnimatedBuilder(animation: _controller, builder: (context, child) => LinearProgressIndicator(
+      value: _curveAnimation.value,
+      valueColor: _colorAnimation,
+      backgroundColor: _colorAnimation.value?.withOpacity(0.4),
+    ));
   }
 }
